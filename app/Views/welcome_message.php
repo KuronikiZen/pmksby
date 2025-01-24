614f02235fe5a2ac43ab31b5a6b53494041aa06c
NatanaelWT <natanaelhdxd25@gmail.com>
2025-01-24 20:29:29 +0700

    upload

---
+++ app/Views/welcome_message.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <!-- <link href="css/app.css" rel="stylesheet" /> -->
+    <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
+  </head>
+  <body>
+    <h1 class="text-2xl font-bold underline">
+      Hello world!
+    </h1>
+  </body>
+</html>
